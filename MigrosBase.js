a603fe572eade4420b54777ea3017c1c7ca63289
NagiMuseibli <nagimuseibli02@gmail.com>
2023-12-29 12:05:06 +0400

    Creating MigrosBase class and Constructor

---
+++ MigrosBase.js
@@ -0,0 +1,8 @@
+class MigrosBase {
+    constructor(firstName, lastName, hasTheCart, products) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.hasTheCart = hasTheCart;
+        this.products = products;
+    }
+}
