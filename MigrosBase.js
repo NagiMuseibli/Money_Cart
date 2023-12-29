999ada15b6ef9d9e93532a6b40883988a06eb760
NagiMuseibli <nagimuseibli02@gmail.com>
2023-12-29 12:15:21 +0400

    Creating calculate and controlerProduct methods

--- MigrosBase.js
+++ MigrosBase.js
@@ -1,8 +1,24 @@
 class MigrosBase {
+    discount_rate = 20;
     constructor(firstName, lastName, hasTheCart, products) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.hasTheCart = hasTheCart;
         this.products = products;
+    }
+
+    calculate() {
+        if (this.controlTheProducts(this.products)) {
+
+        } else {
+            alert("You must buy at least one product");
+        }
+    }
+
+    controlTheProducts(products) {
+        if (products != null && products.length > 0) {
+            return true;
+        }
+        return false;
     }
 }
